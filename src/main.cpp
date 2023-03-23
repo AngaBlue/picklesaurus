811675c995548a9d21b327a947a8abce47d0ee53
AngaBlue <contact@anga.blue>
2023-03-23 16:20:33 +0800

    Add pins header file

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
+#include <pins.h>
+
 //Arduino PWM Speed Control：
 int E1 = 5;  // Motor2 Speed
 int M1 = 4;  // Motor2 Dir
 int E2 = 6;   // Motor1 Speed                   
 int M2 = 7;   // Motor1 Dir 
+
+
 void setup() {
   // put your setup code here, to run once:
 }
@@ -15,6 +19,6 @@ void loop() {
 void motorOnWithSpeed(){
     digitalWrite(M1,LOW);   
     digitalWrite(M2, LOW);       
-    analogWrite(E1,80);//digitalWrite(E1, HIGH);   //PWM Speed Control
-    analogWrite(E2,80);//digitalWrite(E2, HIGH);   //PWM Speed Control 
+    analogWrite(E1,80);
+    analogWrite(E2,80);
 }
