bf443c9f0827e088fd17c21db358bda3259f3f7c
jkie0005 <jonathan@kierce.com>
2023-03-23 14:47:26 +0800

    added motor driving code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
-
+//Arduino PWM Speed Control：
+int E1 = 5;  // Motor2 Speed
+int M1 = 4;  // Motor2 Dir
+int E2 = 6;   // Motor1 Speed                   
+int M2 = 7;   // Motor1 Dir 
 void setup() {
   // put your setup code here, to run once:
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void motorOnWithSpeed(){
+    digitalWrite(M1,LOW);   
+    digitalWrite(M2, LOW);       
+    analogWrite(E1,80);//digitalWrite(E1, HIGH);   //PWM Speed Control
+    analogWrite(E2,80);//digitalWrite(E2, HIGH);   //PWM Speed Control 
 }
