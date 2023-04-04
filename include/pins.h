c5c9ea89d592242957305a12b0241c8bf0c8c52f
AngaBlue <contact@anga.blue>
2023-04-04 18:20:59 +0800

    Remove unused motor pins

--- include/pins.h
+++ include/pins.h
@@ -1,12 +1,6 @@
 /**
  * Motors
  */
-#define MOTOR_SPEED_1 5
-#define MOTOR_DIR_1 4
-
-#define MOTOR_SPEED_2 6                 
-#define MOTOR_DIR_2 7
-
 #define CLAW_SERVO 12
 
 #define PUSH_BUTTON 2
