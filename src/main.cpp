105c8da48c7c0220856e2ec433454adc8bd80a04
jkie0005 <jonathan@kierce.com>
2023-05-11 17:03:04 +0800

    Azimuth

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include <Arduino.h>
 #include <Scoop.hpp>
 #include <Wheels.hpp>
+#include <QMC5883LCompass.h>
 #include <Arm.hpp>
 #include "main.hpp"
 
 static Wheels wheels(SPEED);
 static Scoop scoop;
 static Arm arm;
+static QMC5883LCompass compass;
 
 void setup()
 {
@@ -19,12 +21,13 @@ void setup()
   // Initialise pin modes
   pinMode(PUSH_BUTTON, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-
+  
   // Initialise components
-  scoop.attach(SCOOP_SERVO,SCOOP_UP,SCOOP_DOWN);
-  float multipliers[4] = {WHEEL_1_MULT, WHEEL_2_MULT, WHEEL_3_MULT, WHEEL_4_MULT};
-  wheels.attach(multipliers);
-  arm.attach(ARM_SERVO,ARM_UP,ARM_DOWN);
+  compass.init();
+  //scoop.attach(SCOOP_SERVO,SCOOP_UP,SCOOP_DOWN);
+  //float multipliers[4] = {WHEEL_1_MULT, WHEEL_2_MULT, WHEEL_3_MULT, WHEEL_4_MULT};
+  //wheels.attach(multipliers);
+  //arm.attach(ARM_SERVO,ARM_UP,ARM_DOWN);
 }
 
 void loop()
@@ -39,7 +42,14 @@ void loop()
     wheels.left_reverse();
     wheels.stop();
   } else {
-    scoop.up();
-    arm.down();
+    //scoop.up();
+    //arm.down();
+
+    // Read compass
+    compass.read();
+    int a = compass.getAzimuth();
+#ifdef LOGGING
+    Serial.println(a);
+#endif
   }
 }
