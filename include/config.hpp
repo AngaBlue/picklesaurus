206c3d68413665f8f13e4d6f2a6852d306a79910
AngaBlue <contact@anga.blue>
2023-05-22 06:48:13 +0800

    Label config options

--- include/config.hpp
+++ include/config.hpp
@@ -1,19 +1,25 @@
 #include "pins.hpp"
 
+// Whether to enable logging globally
 #define LOGGING
 
+// Scoop servo positions
 #define SCOOP_START 5
 #define SCOOP_UP 90
 #define SCOOP_DOWN 105
 #define SCOOP_DEPOSIT 175
 #define SCOOP_DEPOSIT_TENNIS 120
 
+// How quickly to wiggle the scoop when depositing squash balls
+#define WIGGLE_TIME 300
+
+// Arm servo positions 
 #define ARM_UP 70
 #define ARM_DOWN 0
 #define ARM_DEPOSIT_TENNIS 160
-#define WIGGLE_TIME 300
-#define SPEED 255
 
+// Wheel speeds
+#define SPEED 255
 #define WHEEL_1_MULT 1.0
 #define WHEEL_2_MULT 1.0
 #define WHEEL_3_MULT 1.0
